c9239d7fe0ddff4918e4a24b3d6683b0dcaa63e9
wrnrlr <werner@alman.ax>
2023-03-10 18:49:23 +0100

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,5 @@
 fn main() {
-    println!("welcome");
-    let text = "hallo";
-    let from = "nl";
-    let to = "en";
-    let result = pg_nlp::get_model(from, to);
-    let (source, target, model) = result.unwrap();
-    println!("{} {}", source, target);
-    let output = model.translate(&[text], source, target);
+    let (source, target, model) = pg_nlp::get_model("nl", "en").unwrap();
+    let output = model.translate(&["hallo"], source, target);
     println!("{}", output.unwrap()[0].to_string());
 }
