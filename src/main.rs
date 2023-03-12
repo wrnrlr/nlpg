33f400560f69b9f0bb0964bf9307c6f07f282845
wrnrlr <werner@alman.ax>
2023-03-12 20:37:31 +0100

    add cargo config, remove main

--- src/main.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let (source, target, model) = pg_nlp::get_model("nl", "en").unwrap();
-    let output = model.translate(&["hallo"], source, target);
-    println!("{}", output.unwrap()[0].to_string());
-}
